6a3f12f9b86710765eb1bbc33da88bad31115a86
estiven2011 <juan_posada23221@elpoli.edu.co>
2023-10-06 18:22:49 -0500

    LOG

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -5,16 +5,38 @@ import "./profile.css"
 function profile() {
   return (
       
-        <div className=" w-full">
-          <div className="profiles w-[20%] h-[79vh] m-5 ">
-            <img src={Profile} alt="" className=' rounded-sm justify-center' />
+        <div className=" w-full flex ">
+          <div className="profiles w-[20%] h-[79vh] m-5 flex">
             <div className=' infoProfile flex flex-col items-center'>
+            <img src={Profile} alt="" className=' rounded-sm justify-center' />
               <p className=' text-center pb-8'><b>Nombre:</b> <br />Jorge Ernesto Espinosa Oviedo</p>
               <p className=' text-center pb-8'><b>Rol:</b> <br />Administrador</p>
               <p className=' text-center pb-8'><b>Correo:</b> <br /> jeespinosa@elpoli.edu.co </p>
             </div>
           </div>
+          <div className=' tableCrud flex flex-col justify-center w-[80%] border-solid border-b-gray-950'>
+              <h1 className=' items-center'>Tabla Maestra de Estudiantes</h1>
+                <table className=' border-2 items-center'>
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>NOMBRE</th>
+                      <th>EDAD</th>
+                      <th>GRADO</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                        <td>ID</td>
+                        <td>Nombre</td>
+                        <td>Edad</td>
+                        <td>Grado</td>
+                      </tr>
+                  </tbody>
+                </table>
+            </div>
         </div>
+        
 
       
   )

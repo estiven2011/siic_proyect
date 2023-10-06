e445276c5db92a075af0e5f4a721cc981852661f
JuanCastao11 <juanmanuelcastano11b@gmail.com>
2023-10-06 18:22:24 -0500

    video

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -5,18 +5,12 @@ import "./profile.css"
 function profile() {
   return (
       
-<<<<<<< HEAD
         <div className=" w-full ">
           <div className="profiles w-[20%]  m-5 ">
-=======
-        <div className=" w-full">
-          <div className="profiles w-[20%] h-[79vh] m-5 ">
->>>>>>> 854c7b38cbb66b6d9e50d9be09a30a15f2c80169
             <img src={Profile} alt="" className=' rounded-sm justify-center' />
             <div className=' infoProfile flex flex-col items-center'>
-              <p className=' text-center pb-8'><b>Nombre:</b> <br />Jorge Ernesto Espinosa Oviedo</p>
-              <p className=' text-center pb-8'><b>Rol:</b> <br />Administrador</p>
-              <p className=' text-center pb-8'><b>Correo:</b> <br /> jeespinosa@elpoli.edu.co </p>
+              <p className=' text-center pb-3'><b>Nombre:</b> <br />Juan Manuel Castaño Agudelo</p>
+              <p className=' text-center '><b>Rol:</b> <br />Administrador</p>
             </div>
           </div>
         </div>

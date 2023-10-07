b6d37ada3ae9bf048d845adba41c2f914f0c57b4
JuanCastao11 <juanmanuelcastano11b@gmail.com>
2023-10-06 20:30:04 -0500

    Hola

--- src/pages/videos/videos.jsx
+++ src/pages/videos/videos.jsx
@@ -9,7 +9,7 @@ function Videos() {
           <a href="#">
             <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
           </a>
-          <div class="p-6">
+          <div class="p-5">
           <img src={Profile} alt="" />
             <a href="#">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Noteworthy technology acquisitions 2021</h5>

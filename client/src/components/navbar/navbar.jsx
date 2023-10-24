4044a94a286abe57228d8dbbcb887fea42034f9e
estiven2011 <juan_posada23221@elpoli.edu.co>
2023-10-23 21:52:37 -0500

    tailwind, navbar

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -7,41 +7,41 @@ import {AiOutlineUser} from "react-icons/ai"
 function Navbar() {
   return (
     <body className=''>
-      <nav className=''>
-        <ul className='  '>
+      <nav className=' flex justify-end bg-[#1e5096] py-6'>
+        <ul className=' duration-300 transition-all font-semibold no-underline flex list-none text-white items-center gap-[5rem] pr-[1%] '>
           <NavLink to="/">
           <li>
-            <a>Home</a>
+            <a className=' hover:border-b-[1px]'>Home</a>
           </li>
           </NavLink>
           <NavLink to="/presentaciones">
           <li>
-            <a>Presentaciones</a>
+            <a className=' hover:border-b-[1px]'>Presentaciones</a>
           </li>
           </NavLink>
           <NavLink to="/videos"> 
           <li>
-            <a>Videos</a>
+            <a className=' hover:border-b-[1px]'>Videos</a>
           </li>
           </NavLink>
           <NavLink to="/tutoriales">
           <li>
-            <a>Tutoriales</a>
+            <a className=' hover:border-b-[1px]'>Tutoriales</a>
           </li>
           </NavLink>
           <NavLink to="/ejercicios">
           <li>
-            <a>Ejercicios</a>
+            <a className=' hover:border-b-[1px]'>Ejercicios</a>
           </li>
           </NavLink>
           <NavLink to="/profile">
           <li>
-            <a> <AiOutlineUser className='profile text-[1.2rem]'/> </a>
+            <a className=' hover:border-b-[1px]'> <AiOutlineUser className='profile text-[1.2rem]'/> </a>
           </li>
           </NavLink>
           <NavLink to="login">
           <li>
-            <a> <FiLogIn className=' text-[1.2rem]'/> </a>
+            <a className=' hover:border-b-[1px]'> <FiLogIn className=' text-[1.2rem]'/> </a>
           </li>
           </NavLink>
         </ul>
